2c8b63865b57d521f28ec310c9b3af742f8c8518
Joseph Anderson <info@jmawebtechnologies.com>
2013-06-14 14:07:15 -0400

    first release
    added code to parse api

---
+++ BigCommerceConsole/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using BigCommerce.RestApi.Core;
+
+namespace BigCommerce.RestApi.ConsoleApp
+{
+    class Program
+    {
+        static string api_key = "";
+        static string username = "";
+        static string baseUrl = "";
+
+        static void Main(string[] args)
+        {
+            // do to product options
+
+            //https://store-4evfk.mybigcommerce.com/api/v2/orders/100/products.xml
+
+            //List<order> orders = GetOrdersByDateRange(DateTime.Now.AddDays(-1), DateTime.Now);
+            //List<Product> p = GetProducts(100);
+            List<Address> add = GetAddresses(100);
+            Console.ReadLine();
+        }
+
+        static List<order> GetOrdersByDateRange(DateTime beginTime, DateTime endTime)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Credentials = new NetworkCredential(username, api_key);
+
+            HttpClient client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            string bt = Rfc822DateTime.ToString(beginTime.ToUniversalTime()).Replace("Z", "-0700").Trim();
+            string et = Rfc822DateTime.ToString(endTime.ToUniversalTime()).Replace("Z", "-0700").Trim();
+
+            string url = String.Format("orders.json?min_date_created={0}&max_date_created={1}", bt, et);
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                List<order> result = response.Content.ReadAsAsync<List<order>>().Result;
+                return result;
+            }
+
+            return null;
+
+        }
+
+        static List<order> GetOrdersByIdRange(int lowestOrder, int highestOrder)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Credentials = new NetworkCredential(username, api_key);
+
+            HttpClient client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            string url = String.Format("orders.json?min_id={0}&max_id={1}", lowestOrder, highestOrder);
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                List<order> result = response.Content.ReadAsAsync<List<order>>().Result;
+                return result;
+            }
+
+            return null;
+        }
+
+        static order GetOrderById(string orderId)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Credentials = new NetworkCredential(username, api_key);
+
+            HttpClient client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            string url = String.Format("orders/{0}.json?", orderId);
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                order result = response.Content.ReadAsAsync<order>().Result;
+                return result;
+            }
+
+            return null;
+        }
+
+        static List<Product> GetProducts(int orderId)
+        {
+            //https://store-4evfk.mybigcommerce.com/
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Credentials = new NetworkCredential(username, api_key);
+
+            HttpClient client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            string url = String.Format("orders/{0}/products.json", orderId);
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                List<Product> result = response.Content.ReadAsAsync<List<Product>>().Result;
+                return result;
+            }
+
+            return null;
+
+        }
+
+        static List<Address> GetAddresses(int orderId)
+        {
+            //https://store-4evfk.mybigcommerce.com/
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.Credentials = new NetworkCredential(username, api_key);
+
+            HttpClient client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            string url = String.Format("orders/{0}/shippingaddresses.json", orderId);
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                List<Address> result = response.Content.ReadAsAsync<List<Address>>().Result;
+                return result;
+            }
+
+            return null;
+
+        }
+    }
+}
